623b2f4114778d74bd560272cc789d8f17d0d010
nerotvlive <daniel.niesmann@outlook.com>
2025-03-02 09:04:05 +0100

    Update to new dependency versions

--- launcher/src/main/java/com/zyneonstudios/nexus/launcher/NexusLauncher.java
+++ launcher/src/main/java/com/zyneonstudios/nexus/launcher/NexusLauncher.java
@@ -1,6 +1,7 @@
 package com.zyneonstudios.nexus.launcher;
 
-import com.zyneonstudios.nexus.desktop.frame.web.NexusWebFrame;
+import com.zyneonstudios.nexus.desktop.frame.nexus.NexusWebFrame;
+import com.zyneonstudios.nexus.desktop.frame.web.NWebFrame;
 import com.zyneonstudios.nexus.desktop.frame.web.NexusWebSetup;
 
 import javax.imageio.ImageIO;
@@ -14,15 +15,22 @@ public class NexusLauncher {
 
     public NexusLauncher(String runDir, String lockDir, String configFile, String launchId, boolean runSilent) {
         if(!runSilent) {
-            setup = new NexusWebSetup("libraries/cef/");
+            setup = new NexusWebSetup(runDir+"/libraries/cef/");
             setup.enableCache(false);
             setup.setup();
-            frame = new NexusWebFrame(setup.getWebClient(),"file:///D:/Workspaces/IntelliJ/nexus-launcher/launcher/src/main/html/main.html",true);
+            frame = new NexusWebFrame(setup.getWebClient(),"file:///home/nerotvlive/IdeaProjects/nexus-launcher/launcher/src/main/html/main.html",true);
             Image icon = null;
             try {
                 icon = ImageIO.read(Objects.requireNonNull(getClass().getResource("/grass.png"))).getScaledInstance(32, 32, Image.SCALE_SMOOTH);
             } catch (Exception ignore) {}
-            frame.setTitlebar("NEXUS Launcher", Color.decode("#131313"), Color.decode("#f5f5f5"), (Image)icon);
+            frame.setTitle("NEXUS Launcher");
+            frame.getTitlebar().setBackground(Color.decode("#131313"));
+            frame.getCloseButton().setBackground(Color.decode("#131313"));
+            frame.getMinimizeButton().setBackground(Color.decode("#131313"));
+            frame.getMinimizeButton().setForeground(Color.decode("#f5f5f5"));
+            frame.getCloseButton().setForeground(Color.decode("#f5f5f5"));
+            frame.getTitlebar().setForeground(Color.decode("#f5f5f5"));
+            frame.setIconImage(icon);
             frame.setMinimumSize(new Dimension(1024,640));
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
